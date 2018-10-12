e2ff04a16d9cf41184db42e81a8a6c002da44396
jnblanchet2 <jnblanchet@suometry.com>
2018-10-12 11:07:43 -0400

    init commit

---
+++ README.txt
@@ -0,0 +1,48 @@
+
+2018-10-10
+
+objective: live sharpness measure
+
+grab frames and display live on screen. PROBLEM: can compiling in terminal, but not in spyder. FIX: run spyder from terminal... now IDE is setup! 
+
+where to find what functions to call? see ids manual here:
+https://en.ids-imaging.com/manuals/uEye_SDK/EN/uEye_Manual_4.91/index.html
+
+also here, for other ueye functions
+C:\Users\demoPC\py\ids-pyueye-master\pyueye\ueye.py
+
+-----------------
+
+2018-10-11 
+
+got the code up running.. used max to track peak sharpness, but noticed that it drifts over time (e.g. went from 0.9 up to 1.7)
+
+improved the data that's displayed on screen
+
+
+
+Todo: 
+- suspect all sharpness values fits a gaussian, so max only shows the extreme cases. to verify, record all sharpness values, and see if they fits a gaussian curve. 
+- possible to come up a formula based on stats, to determine a range for what sharpness value is acceptable.
+- test in dark? 
+- raise exceptions
+- try JN's pattern
+*- automatic tracking using JN's code? 
+
+also, try JN's pattern:
+> cd C:\Users\demoPC\Desktop\SuoCalibration_v2.6.0a\oCametry\application\checkerDetect
+> FindCodedChecker.exe ./boardTag25h7_1.bmp cam  #cam is arbiturary name in case there are three camera present. 
+
+interpretation of the results:
+	tag: -1, c: 5, r: 8, x: 0.000000, y: 0.000000, z: 0.000000
+	FOUND tag: 25, fam: t25h7, detection: 9, rotation: 3, position r: 7, c:3 from board: 0 (0 of target 0), at location r:1, c:2
+	Consistent observation of board 0 (0), size 9 x 6, orientation: 270
+	indexed corners: 54
+	writing 9
+	
+	
+1st col: the Id of the pattern it found (there is only 1 pattern in this file, so it's always 0)
+2nd col: the Id of the corner (maps to the HEADER.dsc file, which describes the pattern and the position of each corner)
+3rd col: 0 if the corner was not seen in the frame (useful for partial occlusion)
+4th col: zero-based row id of the corner (in pixels)
+5th col: zero-based column id of the corner (in pixels)
---
+++ live_focus.py
@@ -0,0 +1,181 @@
+from pyueye import ueye
+import cv2
+import numpy as np
+#from scipy import stats
+#import matplotlib.pyplot as plt
+
+
+def gray(image):
+    return cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+
+def crop(image,r):
+    x,y,delx,dely=r
+    return image[y:y+dely,x:x+delx]
+
+def crop2(image,pts):
+#    x=pts[0][0]
+#    y=pts[0][1]
+#    dely=pts[1][1]-pts[0][1]
+#    delx=pts[1][0]-pts[0][0]
+    return image[pts[0][1]:pts[1][1],pts[0][0]:pts[1][0]]
+
+
+def laplacian(image):
+    return cv2.Laplacian(image,cv2.CV_64F,5)
+
+def edge(image):
+    sy = cv2.Sobel(image,ddepth=cv2.CV_64F,dx=0,dy=1,ksize=5)
+    sx = cv2.Sobel(image,ddepth=cv2.CV_64F,dx=1,dy=0,ksize=5)
+    return (np.power(np.hypot(sx,sy),2))
+    
+def calculate_sharpness(image):
+#    lap = laplacian(image)
+#    sharpness=lap.flatten().var()
+    edg = edge(image)
+    sharpness=edg.flatten().var()
+    return sharpness
+
+def close_all(): 
+    ueye.is_StopLiveVideo(hcam, ueye.IS_FORCE_VIDEO_STOP)
+    ueye.is_ExitCamera(hcam)
+    cv2.destroyAllWindows()
+    print('stop video.. exit camera.. close all windows')    
+    
+def select_roi(image):
+    fromCenter=False
+    h,w,channel = image.shape
+    image_copy = image
+    if h>720:
+        image_copy=cv2.resize(image,(0,0),fx=0.25,fy=0.25)
+    r=cv2.selectROI("select roi please... press ENTER when done", image_copy, fromCenter) #x,y,delx,dely
+    return (int(r[i]*4) for i in range(4))
+
+def draw_rect(event,x,y,flags,param):
+    global pts
+    if event==cv2.EVENT_LBUTTONDOWN:
+        print('start mouse position: %g %g' % (x,y))
+        pts[0]=(x,y)
+  #      pts[1]=(x,y) # to make sure both points are registered before calculating sharpness
+        
+    elif event==cv2.EVENT_LBUTTONUP:
+        print('end mouse position: %g %g' % (x,y))
+        pts[1]=(x,y)
+    
+#def stat(array):
+#    median = np.median(array)
+#    #mode = stats.mode(array, axis=None)
+#    return median
+
+#def auto_find_roi(image):
+   
+    
+
+
+
+def init_cam():
+    global width
+    global height
+    global bitspixel
+    
+    # initialize camera
+    hcam =ueye.HIDS(0)
+    ret = ueye.is_InitCamera(hcam, None)
+    print("camera intialized %g" % ret)
+ 
+    # get fps
+  #  hcam_fps = is_GetFramesPerSecond(hcam, None)
+    
+    # set color mode
+    ueye.is_SetColorMode(hcam, ueye.IS_CM_BGR8_PACKED) 
+    
+    # set region of interest
+    rect_aoi = ueye.IS_RECT()
+    rect_aoi.s32X = ueye.int(0)
+    rect_aoi.s32Y = ueye.int(0)
+    rect_aoi.s32Width = ueye.int(width)
+    rect_aoi.s32Height = ueye.int(height)
+    ueye.is_AOI(hcam, ueye.IS_AOI_IMAGE_SET_AOI, rect_aoi, ueye.sizeof(rect_aoi))
+     
+    
+    # allocate memory
+    mem_ptr = ueye.c_mem_p()
+    mem_id = ueye.int()
+    
+    ueye.is_AllocImageMem(hcam, width, height, bitspixel, mem_ptr, mem_id)
+    ueye.is_SetImageMem(hcam, mem_ptr, mem_id)
+     
+    # continuous capture to memory
+    ueye.is_CaptureVideo(hcam, ueye.IS_DONT_WAIT)
+    
+    return mem_ptr
+    
+    
+if __name__=="__main__":
+    sharpness=0
+    sharps=[] 
+    sharp_max=0 
+    sharp_smooth=0
+    sharp_med=0
+    
+   
+    width = 3088
+    height = 2076
+    bitspixel = 24 # for colormode = IS_CM_BGR8_PACKED
+    
+    
+    x,y,delx,dely = int(width/4),int(height/4),int(width/8),int(height/8)
+    pts=[(0,0),(0,0)]
+     
+    # initialize camera
+    mem_ptr = init_cam()
+    
+    # get data from camera and display
+    lineinc = width * int((bitspixel + 7) / 8)
+ 
+    f = open('data.txt', 'a+')
+    
+    while True:
+        img = ueye.get_data(mem_ptr, width, height, bitspixel, lineinc, copy=True)
+        img = np.reshape(img, (height, width, 3))
+        crp = crop2(img,pts)
+        gry = gray(crp)      
+      
+        if not ((0,0) in pts):
+            sharpness = calculate_sharpness(gry)
+            sharps.append(sharpness)                # write to a var for dbg
+            f.write(str(sharpness))                 # write to a file for dbg
+            sharp_smooth = np.mean(sharps[-100:])
+            sharp_med = np.median(sharps)            
+
+            if sharpness > sharp_max:
+                sharp_max =sharpness    
+                print("sharpest: %.2e" % sharp_max)
+            
+        preview = cv2.resize(img, (0,0), fx=0.5, fy=0.5) 
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        cv2.putText(preview, 'max = %.2e, median=%.2e ' % (sharp_max, sharp_med), (50,50), font, fontScale=1, color=(0,255,0), thickness=2)
+        cv2.putText(preview, 'sharp_smooth = %.2e' % (sharpness), (50, 100), font, fontScale=1, color=(0,255,0), thickness=2)
+        cv2.setMouseCallback('preview',draw_rect)
+        
+        if not ((0,0) in pts):
+            cv2.rectangle(preview, pts[0],pts[1], color=(0,255,0),thickness=2)#, lineType=4)
+
+        cv2.imshow('preview', preview)        
+        
+        if cv2.waitKey(1) & 0xFF == '27':
+            break
+
+    cv2.destroyAllWindows()
+
+'''
+todo:
+   > - add framerate on screen... 
+   x - speed up by analyzing only the cropped region
+   x - either auto find roi based on intensity or pattern matching, or select roi...
+   - try sobel and see if it's this sensitive.. 
+    - collect all sharpness values and change max.. 
+    - try jn's pattern.. auto pattern matching
+    - auto track pattern?
+    
+'''
+    
